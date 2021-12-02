9953513072ba355e167dadfdeb2a75f67cef4280
kashu-02 <hituji_shun@icloud.com>
2021-12-02 10:23:13 +0900

    final

--- okaykasu/countdown.js
+++ okaykasu/countdown.js
@@ -1,9 +1,4 @@
-const dd = document.getElementById('dd')
-const hh = document.getElementById('hh')
-const mm = document.getElementById('mm')
-const ss = document.getElementById('ss')
-const ms = document.getElementById('ms')
-const wk = document.getElementById('wk')
+const cdd = document.getElementById('cd')
 
 const testday = new Date('2022/01/15 09:30:00') 
 
@@ -15,15 +10,10 @@ function countdown (){
     const hour = Math.floor(diff / 1000 / 60 / 60) % 24
     const minutes = Math.floor(diff / 1000 / 60) % 60
     const secound = Math.floor(diff / 1000) % 60
-    const msecond = Math.floor(diff) % 1000
+    const msecond = diff % 1000
     const week = Math.floor(day / 7)
 
-    dd.innerText = day
-    hh.innerText = hour
-    mm.innerText = minutes
-    ss.innerText = secound
-    ms.innerText = msecond
-    wk.innerText = week
+    cd.innerText = `あと${day}日${hour}時間${minutes}分${secound}秒${msecond}（${week}週）`
 }
 
 countdown()
