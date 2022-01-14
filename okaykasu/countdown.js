7af71a2f20104966d801b973d7a2910efb497a4b
kashu-02 <hituji_shun@icloud.com>
2022-01-14 14:55:52 +0900

    Update countdown.js

--- okaykasu/countdown.js
+++ okaykasu/countdown.js
@@ -1,10 +1,17 @@
-const cdd = document.getElementById('cd')
+const cd = document.getElementById('cd')
 
-const testday = new Date('2022/01/15 09:30:00') 
+const testday = new Date('2022/01/15 09:30:00')
+const graduateday = new Date('2022/03/05 09:00:00')
 
 function countdown (){
     const now = new Date()
-    const diff = testday - now
+    let diff
+    if(now <= testday){
+        diff = testday - now
+    }else{
+        diff = graduateday - now
+    }
+    
 
     const day = Math.floor(diff / 1000 / 60 / 60 / 24)
     const hour = Math.floor(diff / 1000 / 60 / 60) % 24
@@ -13,7 +20,11 @@ function countdown (){
     const msecond = diff % 1000
     const week = Math.floor(day / 7)
 
-    cd.innerText = `共通テストまで あと${week}週${day}日${hour}時間${minutes}分${secound}秒${msecond}`
+    if(now <= testday){
+        cd.innerText = `共通テストまで あと${week}週${day}日${hour}時間${minutes}分${secound}秒${msecond}`
+    }else{
+        cd.innerText = `卒業まで あと${week}週${day}日${hour}時間${minutes}分${secound}秒${msecond}`
+    }
 }
 
 countdown()
